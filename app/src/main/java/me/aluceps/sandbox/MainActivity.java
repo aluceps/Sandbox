a6179fe4b0612f5d70ef311b6527778a811af7bb
takano <takano@echoes.co.jp>
2017-10-12 17:48:58 +0900

    add databinding

--- app/src/main/java/me/aluceps/sandbox/MainActivity.java
+++ app/src/main/java/me/aluceps/sandbox/MainActivity.java
@@ -1,13 +1,18 @@
 package me.aluceps.sandbox;
 
-import android.support.v7.app.AppCompatActivity;
+import android.databinding.DataBindingUtil;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import me.aluceps.sandbox.databinding.ActivityMainBinding;
 
 public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
     }
 }
