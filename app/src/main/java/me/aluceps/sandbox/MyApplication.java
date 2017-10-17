1b5510815affad6434f5f4ad8f9c4bb9e37ef72c
aluceps <aluceps@gmail.com>
2017-10-17 12:08:49 +0900

    最低限のアプリケーションクラスの実装

--- app/src/main/java/me/aluceps/sandbox/MyApplication.java
+++ app/src/main/java/me/aluceps/sandbox/MyApplication.java
@@ -2,10 +2,26 @@ package me.aluceps.sandbox;
 
 import android.app.Application;
 
+import timber.log.Timber;
+
 public class MyApplication extends Application {
+
+    private static Application application;
 
     @Override
     public void onCreate() {
         super.onCreate();
+        application = this;
+        initializeTimber();
+    }
+
+    private void initializeTimber() {
+        if (BuildConfig.DEBUG) {
+            Timber.plant(new Timber.DebugTree());
+        }
+    }
+
+    public static Application getApplication() {
+        return application;
     }
 }
