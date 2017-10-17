1bd2feef2a03d10e5e67152c656b59f2c5699bf0
aluceps <aluceps@gmail.com>
2017-10-17 12:22:04 +0900

    シンプルに画面を表示するだけの実装

---
+++ app/src/main/java/me/aluceps/sandbox/view/MainFragment.java
@@ -0,0 +1,39 @@
+package me.aluceps.sandbox.view;
+
+import android.databinding.DataBindingUtil;
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import me.aluceps.sandbox.R;
+import me.aluceps.sandbox.databinding.FragmentMainBinding;
+
+public class MainFragment extends Fragment {
+
+    private FragmentMainBinding binding;
+
+    public static MainFragment newInstance() {
+        MainFragment fragment = new MainFragment();
+        return fragment;
+    }
+
+    @Override
+    public void onCreate(@Nullable Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+    }
+
+    @Nullable
+    @Override
+    public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        binding = DataBindingUtil.inflate(inflater, R.layout.fragment_main, container, false);
+        return binding.getRoot();
+    }
+
+    @Override
+    public void onViewCreated(View view, @Nullable Bundle savedInstanceState) {
+        super.onViewCreated(view, savedInstanceState);
+    }
+}
